7c4721ed5284873b4a293ee978aa335b32d1b5ff
GyanendraMohan <gmpatel152@gmail.com>
2023-10-30 17:50:48 +0530

    added todays deal section

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -141,7 +141,58 @@ const HomeScreen = () => {
             </Pressable>
           ))}
         </View>
-        <Text style={{height: 1, borderColor:"#D0D0D0", borderWidth: 2, marginTop: 15}}/>
+        <Text
+          style={{
+            height: 1,
+            borderColor: "#D0D0D0",
+            borderWidth: 2,
+            marginTop: 15,
+          }}
+        />
+
+        <Text style={{ padding: 10, fontSize: 18, fontWeight: "bold" }}>
+          Today's Deals
+        </Text>
+
+        <ScrollView horizontal showsVerticalScrollIndicator={false}>
+          {offers.map((item, index) => (
+            <Pressable
+              key={index}
+              style={{
+                marginVertical: 10,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Image
+                source={{ uri: item?.image }}
+                style={{ width: 150, height: 150, resizeMode: "contain" }}
+              />
+              <View
+                style={{
+                  backgroundColor: "#E31837",
+                  paddingVertical: 5,
+                  width: 130,
+                  justifyContent: "center",
+                  alignItems: "center",
+                  marginTop: 10,
+                  borderRadius: 4,
+                }}
+              >
+                <Text
+                  style={{
+                    textAlign: "center",
+                    color: "white",
+                    fontSize: 13,
+                    fontWeight: "bold",
+                  }}
+                >
+                  {item?.offer}
+                </Text>
+              </View>
+            </Pressable>
+          ))}
+        </ScrollView>
       </ScrollView>
     </SafeAreaView>
   );
