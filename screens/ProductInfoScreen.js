2eb73e155d5e04c74a083b17211a65dcb6b35a46
GyanendraMohan <gmpatel152@gmail.com>
2023-11-01 18:37:31 +0530

    completed produuct info screen page

--- screens/ProductInfoScreen.js
+++ screens/ProductInfoScreen.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { StyleSheet, Text, ScrollView, Pressable, View, TextInput, ImageBackground, Dimensions } from 'react-native'
 import { AntDesign } from "@expo/vector-icons";
 import { Entypo } from "@expo/vector-icons";
+import { Ionicons } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { useRoute } from '@react-navigation/native';
 
@@ -58,6 +59,41 @@ const ProductInfoScreen = () => {
               </ImageBackground>
             ))}
       </ScrollView>
+      <View style={{padding:10}}>
+          <Text style={{fontSize: 15, fontWeight: "500"}}>
+            {route?.params?.title}
+          </Text>
+          <Text style={{fontSize: 18, fontWeight:"800", marginTop:6}}>
+            {route?.params?.price} $
+          </Text>
+      </View>
+      <Text style={{height: 1, borderColor:"#D0D0D0", borderWidth:1}}/>
+      <View style={{flexDirection:"row", alignItems:"center", padding:10}}>
+          <Text>Color: </Text>
+          <Text style={{fontSize: 15,  fontWeight:"bold"}}>{route?.params?.color}</Text>
+      </View>
+      <View style={{flexDirection:"row", alignItems:"center", padding:10}}>
+          <Text>Size: </Text>
+          <Text style={{fontSize: 15,  fontWeight:"bold"}}>{route?.params?.size}</Text>
+      </View>
+      <Text style={{height: 1, borderColor:"#D0D0D0", borderWidth:1}}/>
+      <View style={{padding: 10}}>
+              <Text style={{fontSize: 15, fontWeight: "bold", marginVertical: 5}}>Total: {route.params.price} $</Text>
+              <Text style={{color:"#00CED1"}}>FREE delivery tomorow by 3 PM. Order within 10hrs 30 mins</Text>
+              <View style={{flexDirection:"row", marginVertical:5, alignItems:"center", gap: 5}}>
+                  <Ionicons name="location" size={24} color="black" />
+                  <Text style={{fontSize: 15, fontWeight: "500"}}>Deliver To Gyanendra - Varanasi, 221204</Text>
+              </View>
+      </View>
+      <Text style={{color:"green", marginHorizontal:10, fontWeight:"500"}}>
+        In Stock
+      </Text>
+      <Pressable style={{backgroundColor:"#FFC72C", padding: 10, borderRadius: 20, justifyContent:"center", alignItems:"center", marginHorizontal: 10, marginVertical: 10}}>
+        <Text>Add to Cart</Text>
+      </Pressable>
+      <Pressable style={{backgroundColor:"#FFEC1C", padding: 10, borderRadius: 20, justifyContent:"center", alignItems:"center", marginHorizontal: 10, marginVertical: 10}}>
+        <Text>Buy Now</Text>
+      </Pressable>
     </ScrollView>
   )
 }
